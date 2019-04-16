a8df1a3930c0f83ab0b8993eaed0b81884839f6e
Isaac Abramowitz <isaacabramowitz1997@gmail.com>
2019-04-15 21:42:34 -0400

    Create interface '+Game'

---
+++ PrisonersDilemna/src/main/java/com/isaacabramowitz/prisonersdilemna/interfaces/Game.java
@@ -0,0 +1,47 @@
+package com.isaacabramowitz.prisonersdilemna.interfaces;
+
+/**
+ * Represents an iterative game of prisoner's dilemma
+ *
+ * @author Isaac Abramowitz
+ * @version 2019-04-15
+ */
+public interface Game {
+
+    /**
+     * Starts a series of games of prisoner's dilemna
+     *
+     * @param turns amount of turns to be played in the match
+     * @param player1 the first Player
+     * @param player2 the second Player
+     */
+    public void start(int turns, Player player1, Player player2);
+
+    /**
+     * Returns player1's score
+     *
+     * @return the amount of points they scored
+     */
+    public int getPlayer1Results();
+
+    /**
+     * Returns player2's score
+     *
+     * @return the amount of points they scored
+     */
+    public int getPlayer2Results();
+
+    /**
+     * Returns the Player object for the 1st player
+     *
+     * @return the 1st Player
+     */
+    public Player getPlayer1();
+
+    /**
+     * Returns the Player object of the 2nd player
+     *
+     * @return the 2nd Player
+     */
+    public Player getPlayer2();
+}
