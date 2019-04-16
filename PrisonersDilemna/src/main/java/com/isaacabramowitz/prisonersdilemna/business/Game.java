1ea092eda9ee5bdb27b23513c116d5f35cf0607d
Isaac Abramowitz <isaacabramowitz1997@gmail.com>
2019-04-15 21:46:07 -0400

    Create class '+Game'

---
+++ PrisonersDilemna/src/main/java/com/isaacabramowitz/prisonersdilemna/business/Game.java
@@ -0,0 +1,39 @@
+package com.isaacabramowitz.prisonersdilemna.business;
+
+import com.isaacabramowitz.prisonersdilemna.interfaces.Playable;
+import com.isaacabramowitz.prisonersdilemna.interfaces.Player;
+
+/**
+ * Represents an iterative game of prisoner's dilemma
+ *
+ * @author Isaac Abramowitz
+ * @version 2019-04-15
+ */
+public class Game implements Playable {
+
+    @Override
+    public void start(int turns, Player player1, Player player2) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int getPlayer1Results() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int getPlayer2Results() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Player getPlayer1() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Player getPlayer2() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
