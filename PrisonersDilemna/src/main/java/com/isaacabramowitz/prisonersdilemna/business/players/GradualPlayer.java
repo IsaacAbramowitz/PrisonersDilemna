fb33eff081f96ae05bc7363e6c5298b4e483acca
Isaac Abramowitz <isaacabramowitz1997@gmail.com>
2019-04-16 21:02:58 -0400

    Create class '+GradualPlayer'

---
+++ PrisonersDilemna/src/main/java/com/isaacabramowitz/prisonersdilemna/business/players/GradualPlayer.java
@@ -0,0 +1,23 @@
+package com.isaacabramowitz.prisonersdilemna.business.players;
+
+import com.isaacabramowitz.prisonersdilemna.interfaces.Player;
+
+/**
+ * Player who defects according to the amount of times their opponent has
+ * defected
+ *
+ * @author Isaac Abramowitz
+ * @version 2019-04-16
+ */
+public class GradualPlayer implements Player {
+
+    @Override
+    public boolean choose() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean choose(boolean opponentPreviousChoice) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
