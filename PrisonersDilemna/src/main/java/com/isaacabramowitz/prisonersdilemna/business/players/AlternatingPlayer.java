c752903fea9fdcfcbca41a70a415ff5e8051b3a4
Isaac Abramowitz <isaacabramowitz1997@gmail.com>
2019-04-16 20:51:05 -0400

    Create class '+AlternatingPlaye'

---
+++ PrisonersDilemna/src/main/java/com/isaacabramowitz/prisonersdilemna/business/players/AlternatingPlayer.java
@@ -0,0 +1,22 @@
+package com.isaacabramowitz.prisonersdilemna.business.players;
+
+import com.isaacabramowitz.prisonersdilemna.interfaces.Player;
+
+/**
+ * Player that alternates between cooperating and defecting
+ *
+ * @author Isaac Abramowitz
+ * @version 2019-04-16
+ */
+public class AlternatingPlayer implements Player {
+
+    @Override
+    public boolean choose() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean choose(boolean opponentPreviousChoice) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
