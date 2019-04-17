4ab438963e54f155f003675546c94cc32c42e337
Isaac Abramowitz <isaacabramowitz1997@gmail.com>
2019-04-16 21:27:59 -0400

    Create class '+ReverseAlternatingPlayer'

---
+++ PrisonersDilemna/src/main/java/com/isaacabramowitz/prisonersdilemna/business/players/ReverseAlternatingPlayer.java
@@ -0,0 +1,22 @@
+package com.isaacabramowitz.prisonersdilemna.business.players;
+
+import com.isaacabramowitz.prisonersdilemna.interfaces.Player;
+
+/**
+ * Player who alternates between defecting and cooperating
+ *
+ * @author Isaac Abramowitz
+ * @version 2019-04-16
+ */
+public class ReverseAlternatingPlayer implements Player {
+
+    @Override
+    public boolean choose() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean choose(boolean opponentPreviousChoice) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
