7058fdf94bc71e573a060e71d616e38cbd99ddf3
Isaac Abramowitz <isaacabramowitz1997@gmail.com>
2019-04-16 21:03:45 -0400

    Override method '+choose():boolean'

--- PrisonersDilemna/src/main/java/com/isaacabramowitz/prisonersdilemna/business/players/GradualPlayer.java
+++ PrisonersDilemna/src/main/java/com/isaacabramowitz/prisonersdilemna/business/players/GradualPlayer.java
@@ -13,7 +13,7 @@ public class GradualPlayer implements Player {
 
     @Override
     public boolean choose() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return true;
     }
 
     @Override
