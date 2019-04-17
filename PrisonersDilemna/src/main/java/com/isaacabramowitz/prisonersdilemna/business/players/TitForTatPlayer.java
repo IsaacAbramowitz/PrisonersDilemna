a53466b9802734fed6fcbad1773019a168aa284c
Isaac Abramowitz <isaacabramowitz1997@gmail.com>
2019-04-16 21:34:06 -0400

    Override method '+choose(boolean):boolean'

--- PrisonersDilemna/src/main/java/com/isaacabramowitz/prisonersdilemna/business/players/TitForTatPlayer.java
+++ PrisonersDilemna/src/main/java/com/isaacabramowitz/prisonersdilemna/business/players/TitForTatPlayer.java
@@ -17,6 +17,6 @@ public class TitForTatPlayer implements Player {
 
     @Override
     public boolean choose(boolean opponentPreviousChoice) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return this.choose();
     }
 }
