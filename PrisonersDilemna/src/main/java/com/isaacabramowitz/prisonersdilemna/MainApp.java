24551f3b7ec0b35ffea0bcba6eb2e9b0839ef083
Isaac Abramowitz <isaacabramowitz1997@gmail.com>
2019-04-20 00:04:48 -0400

    Create class '+MainApp'

---
+++ PrisonersDilemna/src/main/java/com/isaacabramowitz/prisonersdilemna/MainApp.java
@@ -0,0 +1,14 @@
+package com.isaacabramowitz.prisonersdilemna;
+
+/**
+ * Main app to run Games
+ *
+ * @author Isaac Abramowitz
+ * @version 2019-04-19
+ */
+public class MainApp {
+
+    public static void main(String[] args) {
+
+    }
+}
