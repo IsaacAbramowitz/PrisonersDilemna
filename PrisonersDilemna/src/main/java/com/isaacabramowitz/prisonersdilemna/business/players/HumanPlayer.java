086152b41769077a94e24976f79a5a55ceedd256
Isaac Abramowitz <isaacabramowitz1997@gmail.com>
2019-04-19 23:43:46 -0400

    Create class '+HumanPlayer'

---
+++ PrisonersDilemna/src/main/java/com/isaacabramowitz/prisonersdilemna/business/players/HumanPlayer.java
@@ -0,0 +1,22 @@
+package com.isaacabramowitz.prisonersdilemna.business.players;
+
+import com.isaacabramowitz.prisonersdilemna.interfaces.Player;
+
+/**
+ * A Player that gets user input to decide whether to cooperate or defect
+ *
+ * @author Isaac Abramowitz
+ * @version 2019-04-19
+ */
+public class HumanPlayer implements Player {
+
+    @Override
+    public boolean choose() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean choose(boolean opponentPreviousChoice) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
