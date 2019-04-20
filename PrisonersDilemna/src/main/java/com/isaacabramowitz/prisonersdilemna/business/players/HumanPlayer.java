115fc714e1225dc4f9891a2a5a758054d0e01ef7
Isaac Abramowitz <isaacabramowitz1997@gmail.com>
2019-04-19 23:50:24 -0400

    Override method '+choose(boolean):boolean'

--- PrisonersDilemna/src/main/java/com/isaacabramowitz/prisonersdilemna/business/players/HumanPlayer.java
+++ PrisonersDilemna/src/main/java/com/isaacabramowitz/prisonersdilemna/business/players/HumanPlayer.java
@@ -42,6 +42,6 @@ public class HumanPlayer implements Player {
 
     @Override
     public boolean choose(boolean opponentPreviousChoice) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return this.choose();
     }
 }
